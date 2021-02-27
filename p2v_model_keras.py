cc6a95f9f279a32590879dc6a70e23015ad24191
surajbidnur <63632653+surajbidnur@users.noreply.github.com>
2021-02-27 11:06:53 +0530

    Modified datagen. Added bunch of TODO's to look at. Cleaned up code little

--- p2v_model_keras.py
+++ p2v_model_keras.py
@@ -1,3 +1,5 @@
+# TODO: after all other TODO's are sorted, start making modules of the code and adding comments and docstrings
+
 import tensorflow as tf
 import json
 import os
@@ -9,8 +11,6 @@ import binvox_rw
 from PIL import Image
 import cv2
 from tensorflow.keras.applications.vgg16 import VGG16
-from tqdm.keras import TqdmCallback
-from learning_rate import lr_scheduler
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -20,15 +20,11 @@ TAXONOMY_FILE_PATH    = 'C:\\Users\\bidnu\\Documents\\Suraj_Docs\\3D_Project\\Sh
 RENDERING_PATH        = 'C:\\Users\\bidnu\\Documents\\Suraj_Docs\\3D_Project\\ShapeNet_P2V\\ShapeNetRendering\\{}\\{}\\rendering'
 VOXEL_PATH            = 'C:\\Users\\bidnu\\Documents\\Suraj_Docs\\3D_Project\\ShapeNet_P2V\\ShapeNetVox32\\{}\\{}\\model.binvox'
 
-# global test_iou
-# global mean_iou
-
 with open(TAXONOMY_FILE_PATH, encoding='utf-8') as file:
   taxonomy_dict = json.loads(file.read())
 
 def get_xy_paths(taxonomy_dict, mode = 'train'):
   path_list = []
-  temp = []
   for i in range(len(taxonomy_dict)):
     for sample in taxonomy_dict[i][mode]:
       render_txt = os.path.join(RENDERING_PATH.format(taxonomy_dict[i]["taxonomy_id"], sample), "renderings.txt")
@@ -47,54 +43,61 @@ def get_xy_paths(taxonomy_dict, mode = 'train'):
   return path_list
 
 train_path_list = get_xy_paths(taxonomy_dict = taxonomy_dict, mode = 'train')
-# print(sys.getsizeof(train_path_list), "bytes")
-# print(len(train_path_list))
-# train_path_list = np.asarray(train_path_list)
-# print("------------------------------------------------------------------------")
+train_path_list_sample = train_path_list[:500] + train_path_list[-500:] # just for testing purposes
 
 #Implementation 1 (correct)
+# TODO: the output of this function for some reason cannot be appended to a list in the trianing loop
+# check the TODO comment there also
+# I tried replicating the function in numpy but that also didn't solve the issue
+# And we need to take care when we pass batch inputs. right now I have added a crude implementation of that but try and make it better
+
 def calc_iou_loss(y_true, y_pred):
-  y_true = tf.convert_to_tensor(y_true)
-  y_pred = tf.convert_to_tensor(y_pred)
+  # y_true = tf.convert_to_tensor(y_true)
+  # y_pred = tf.convert_to_tensor(y_pred)
 
-  _volume = tf.cast(tf.math.greater_equal(y_pred, 0.3), dtype = tf.float32)
-  # print(_volume)
+  res = []
+  for i in range(y_true.shape[0]):
+    # _volume = tf.cast(tf.math.greater_equal(y_pred, 0.3), dtype = tf.float32)
+    # print(_volume)
+    _volume = np.greater_equal(y_pred[i], 0.3).astype(np.float32)
+    # print(_volume)
 
-  a = tf.math.multiply(_volume, y_true)
-  b = tf.math.reduce_sum(a)
-  intersection = tf.cast(b, dtype = tf.float32)
+    # a = tf.math.multiply(_volume, y_true)
+    a = np.multiply(_volume, y_true[i])
+    # b = tf.math.reduce_sum(a)
+    b = np.sum(a)
+    # intersection = tf.cast(b, dtype = tf.float32)
+    # intersection = b.astype(np.float32)
 
-  c = tf.math.add(_volume,y_true)
-  d = tf.cast(tf.math.greater_equal(c, 1), dtype = tf.float32)
-  # print(d)
-  e = tf.math.reduce_sum(d)
-  union = tf.cast(e, dtype = tf.float32)
+    # c = tf.math.add(_volume,y_true)
+    c = np.add(_volume, y_true[i])
+    # d = tf.cast(tf.math.greater_equal(c, 1), dtype = tf.float32)
+    d = np.greater_equal(c, 1).astype(np.float32)
 
-  iou = (intersection / union)
-  
-  # IoU per taxonomy
-  # test_iou = dict()
-  # for i in sample:
-  #   if i not in test_iou:
-  #     test_iou[i] = {'n_samples': 0, 'iou': []}
-  #   test_iou[i]['n_samples'] += 1
-  #   test_iou[i]['iou'].append(iou.numpy().tolist())
-  
-  # # Mean IoU
-  # mean_iou = []
-  # for taxonomy_id in test_iou:
-  #   test_iou[taxonomy_id]['iou'] = np.mean(test_iou[taxonomy_id]['iou'], axis=0)
-    # mean_iou.append(test_iou[taxonomy_id]['iou'] * test_iou[taxonomy_id]['n_samples'])
+    # print(d)
+    # e = tf.math.reduce_sum(d)
+    e = np.sum(d)
+    # union = tf.cast(e, dtype = tf.float32)
+    # union = e.astype(np.float32)
 
-  # TODO: n_samples not defined. Check
-  # mean_iou = np.sum(mean_iou, axis=0) / n_samples
+    # iou = (intersection / union)
+    iou = np.divide(b,e)
+    res.append(iou.tolist())
+
+  # ans = np.array(res).reshape(-1,1)
    
-  return iou
+  return res
 
 # y_true = np.random.randint(0,2,size=(32, 32, 32)).astype(np.float32)
 # y_pred = np.random.random(size=(32,32,32)).astype(np.float32)
 
-# print("iou - {}".format(calc_iou_loss(y_true, y_pred)))
+# ans = calc_iou_loss(y_true, y_pred)
+# print(ans)
+# print("iou - {}".format(ans))
+# temp = []
+# temp.append(ans)
+# temp.append(ans)
+# print(temp)
 
 def tf_data_generator(file_list, batch_size=16):
   i = 0
@@ -141,43 +144,25 @@ def tf_data_generator(file_list, batch_size=16):
     # print(img.itemsize)
     # print(target.itemsize)
     yield img, target, sample
-    # del img
-    # del target
-    # del sample
     i = i + 1
 
-# x,y = next(tf_data_generator(train_path_list))
-# print(x.shape)
-# print(y.shape)
+# TODO: look at data augmentation becuase there is a class imbalance of images, the airplane class has a lot of images
+# but the bench doesnt have so many
+# So try and figure out how to add data augmentation
 
-# def fake_decoder_generator(batch_size=16):
-#   while(1):
-#     encoder_output = np.random.random(size=(4,4,128))
-#     encoder_output = np.repeat(encoder_output[np.newaxis,:,:,:],batch_size,axis=0)
-#     encoder_output = np.reshape(encoder_output, (-1,4,4,128))
+def tf_data_generator2(file_list):
+  for img, voxel, tax_id in file_list:
+    rgba_in = Image.open(img)
+    # rgba_in.load()
+    background = Image.new("RGB", rgba_in.size, (255, 255, 255))
+    background.paste(rgba_in, mask=rgba_in.split()[3]) # 3 is the alpha channel
+    rendering_image = cv2.resize(np.array(background).astype(np.float32), (224,224)) / 255.
 
-#     decoder_output = np.random.randint(0,2,size=(32,32,32))
-#     decoder_output = np.repeat(decoder_output[np.newaxis,:,:,:],batch_size,axis=0)
-#     decoder_output = np.reshape(decoder_output, (-1,32,32,32))
+    with open(voxel, 'rb') as f:
+      volume = binvox_rw.read_as_3d_array(f)
+      volume = volume.data.astype(np.float32)
 
-#     yield encoder_output, decoder_output
-
-# x,y = next(fake_decoder_generator(8))
-# print(x.shape)
-# print(y.shape)
-
-# batch_size = 2
-# dataset = tf.data.Dataset.from_generator(tf_data_generator,args= [train_path_list, batch_size],
-#                                         output_types = (tf.float32, tf.float32),
-#                                         output_shapes = ((None,224,224,3),(None,32,32,32)))
-
-# batch_size = 8
-# dataset = tf.data.Dataset.from_generator(fake_decoder_generator,args= [batch_size],
-#                                         output_types = (tf.float32, tf.int8),
-#                                         output_shapes = ((None,4,4,128),(None,32,32,32)))
-
-# print(len(train_path_list))
-# steps_per_epoch = len(train_path_list) // batch_size
+    yield rendering_image, volume, tax_id
 
 def encoder(inp, input_shape=(224,224,3)):
   vgg = VGG16(include_top = False,
@@ -279,17 +264,14 @@ if __name__ == '__main__':
                         name = "input_layer")
   encoder_model = tf.keras.Model(input, encoder(input), name = "encoder")
   # encoder_model.summary()
-  batch_size = 1
-  # print(keras_model_memory_usage_in_bytes(encoder_model, batch_size))
-  print("-------------------------")
+  # print("-------------------------")
 
   decoder_input = tf.keras.Input(shape=(2, 2, 2, 256),
                                 name = "decoder_input")
 
   decoder_model = tf.keras.Model(decoder_input, decoder(decoder_input), name = "decoder")
   # decoder_model.summary()
-  # print(keras_model_memory_usage_in_bytes(decoder_model, batch_size))
-  print("-------------------------")
+  # print("-------------------------")
 
   # Autoencoder
   encoder_output = encoder_model(input)
@@ -298,70 +280,59 @@ if __name__ == '__main__':
 
   autoencoder_model = tf.keras.Model(input, decoder_model(decoder_input), name ='autoencoder')
   autoencoder_model.summary()
+  # print("-------------------------")
 
   # Loss function
   loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits = True)
 
-  # Metric
-  # metric = tf.keras.metrics.BinaryCrossentropy(name="binary_crossentropy")
-  # need to add intersection over union here as metric
+  # learning rate scheduler. learning rate becomes 0.01*0.5 after 150 epochs else it is 0.01*1.0
+  learning_rate = 0.001
+  boundaries = [150]
+  values = [1.0, 0.5]
+  learning_rate_fn = tf.keras.optimizers.schedules.PiecewiseConstantDecay(boundaries, values)
 
   #  optimizer
-  # TODO: Add learning rate scheduler to custom training loop
   opt = tf.keras.optimizers.Adam()
 
 #   # compile_model
 #   autoencoder_model.compile(optimizer = opt, loss = loss_fn, metrics = [calc_iou_loss])
   
   # TODO: third output shape not defined properly. Check. Related to IoU calculation
-  train_dataset = tf.data.Dataset.from_generator(tf_data_generator,args= [train_path_list, batch_size],
-                                          output_types = (tf.float32, tf.float32, tf.string),
-                                          output_shapes = ((None, 224, 224, 3),(None, 32, 32, 32),(None, 1)))
-  train_dataset.prefetch(tf.data.AUTOTUNE)
-#   # print(len(train_path_list))
-#   steps_per_epoch = len(train_path_list) // batch_size
-
-# #   # fit model
-# #   autoencoder_model.fit(dataset, epochs = 5, verbose = 0,
-# #                         steps_per_epoch = steps_per_epoch,
-# #                         workers = 4,
-# #                         use_multiprocessing = True,
-# #                         callbacks=[TqdmCallback(verbose=2)])
-
-#   # Callbacks
-#   callbacks = []
-#   # Checkpoint
-#   filepath = "saved-model-{epoch:02d}-{val_acc:.2f}.hdf5"
-#   checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False, save_weights_only=False, mode='max', period = 1)
-#   callbacks.append(checkpoint)
-
-#   # LR Scheduler
-#   callbacks.append(tf.keras.callbacks.LearningRateScheduler(lr_scheduler, verbose=1))
-
-#   # fit model
-#   autoencoder_model.fit(dataset, 
-#   						          epochs = 5, 
-#   						          verbose = 0,
-#                         steps_per_epoch = steps_per_epoch,
-#                         workers = 4,
-#                         use_multiprocessing = True,
-#                         callbacks=callbacks)
+  # train_dataset = tf.data.Dataset.from_generator(tf_data_generator,args= [train_path_list_sample, batch_size],
+  #                                         output_types = (tf.float32, tf.float32, tf.string),
+  #                                         output_shapes = ((None, 224, 224, 3),(None, 32, 32, 32),(None, 1)))
+  # train_dataset.prefetch(tf.data.AUTOTUNE).cache("cache_file.txt")
   
-  # # TODO: Do we need these as global anymore?
-  # global test_iou
-  # global mean_iou
+  batch_size = 1
+
+  # TODO: since the older generator was running forever, this is a fix but need to check propperly again
+  train_dataset = tf.data.Dataset.from_generator(tf_data_generator2,args= [train_path_list_sample],
+                                          output_types = (tf.float32, tf.float32, tf.string))
+  train_dataset = train_dataset.batch(batch_size).shuffle(100).prefetch(tf.data.AUTOTUNE)
+
+  # for x,y,z in train_dataset.take(5):
+  #   print(x.shape, y.shape, z.shape)
+  
+  mean_iou = list()
+  mean_class_iou = list()
 
   # TODO: add checkpoint to custom training loop
-  num_training_samples = len(train_path_list)
-  epochs = 2
-  for epoch in range(1, epochs+1):
+  num_training_samples = len(train_path_list_sample)
+  epochs = 4
+  test_iou = dict()
+  for epoch in range(epochs):
     print("\nepoch {}/{}".format(epoch + 1,epochs))
+    learning_rate = learning_rate_fn(epoch)
 
-    progBar = tf.keras.utils.Progbar(num_training_samples, stateful_metrics=['loss_fn'])
+    progBar = tf.keras.utils.Progbar(num_training_samples, stateful_metrics=['loss_fn'], verbose=1)
 
     # Iterate over the batches of the dataset.
     for step, (x_batch_train, y_batch_train, tax_id) in enumerate(train_dataset):
-      tax_id = tax_id.numpy().tolist()
+
+      tax_id = tax_id.numpy()
+      tax_id = [item.decode("utf-8") for item in tax_id] # byte string (b'hello' to regular string 'hello')
+
+      # print(learning_rate)
 
       # Open a GradientTape to record the operations run
       # during the forward pass, which enables auto-differentiation.
@@ -384,50 +355,58 @@ if __name__ == '__main__':
       # the value of the variables to minimize the loss.
       opt.apply_gradients(zip(grads, autoencoder_model.trainable_weights))
 
+      # print(learning_rate)
       iou = calc_iou_loss(y_batch_train, logits)
 
-      tax_id = [item for items in tax_id for item in items]
-
       # TODO: for some reason test_iou has to be defined here itself otherwise getting an error. Check
-      test_iou = dict()
-      for i in tax_id:
-        if i not in test_iou:
-          test_iou[i] = {'n_samples': 0, 'iou': []}
-        test_iou[i]['n_samples'] += 1
-        test_iou[i]['iou'].append(iou.numpy().tolist())
+      # test_iou = dict()
+      for i,j in enumerate(tax_id):
+        # print(i)
+        if j not in test_iou:
+
+          # TODO: not able to append the iou list to the iou filed of the dictionary below. This is a temporary workaround
+          # @dhruv @rishab try fixing this issue
+
+          test_iou[j] = {'n_samples': 0, 'iou': 0.0}
+
+        # TODO: the counting of n_samples doesn't reset to 0 after an epoch, so check this also
+        test_iou[j]['n_samples'] += 1
+        test_iou[j]['iou'] += iou[i]
       
-      # Mean IoU
-      mean_iou = []
+      # # Mean IoU
+      # mean_iou = []
       for taxonomy_id in test_iou:
-        test_iou[taxonomy_id]['iou'] = np.mean(test_iou[taxonomy_id]['iou'], axis=0)
-      
-      mean_class_iou = []
+        test_iou[taxonomy_id]['iou'] = test_iou[taxonomy_id]['iou'] / test_iou[taxonomy_id]['n_samples']
+
+      # mean_class_iou = []
       for taxonomy_id in test_iou:
-        mean_class_iou.append(test_iou[taxonomy_id]['iou'])
-      # mean_class_iou = json.loads(mean_class_iou) #JSONify the mean iou list containing mean iou for each class
-      mean_class_iou = json.dumps(mean_class_iou) #JSONify the mean iou list containing mean iou for each class
+        mean_iou.append(test_iou[taxonomy_id]['iou'])
+      # # mean_class_iou = json.loads(mean_class_iou) #JSONify the mean iou list containing mean iou for each class
+      # mean_class_iou = json.dumps(mean_class_iou) #JSONify the mean iou list containing mean iou for each class
 
       # TODO: not able to access the test_iou or mean_iou variables even though they are global. Check
-      values=[('train_loss',loss_value),('IoU_metric',iou)]
-
-      # the if loop is just for dubugging purposes
-      # if step*batch_size == 500:
-      #   break
+      values=[('train_loss',loss_value)]
       
-      # FOR TESTING -> Uncomment next if clause to check if the model can be saved and loaded
-      if step % 500 == 0:
-        print("[STEP = %d] --> [TRAINING LOSS = %.4f] --> [TRAINING IOU = %s]" % (step, float(loss_value), mean_class_iou))
-        file_path = 'ae_model_step_{}.h5'.format(step)
-        tf.keras.models.save_model(model = autoencoder_model, filepath = file_path, overwrite = False, include_optimizer = True)
+      # # FOR TESTING -> Uncomment next if clause to check if the model can be saved and loaded
+      # if step % 500 == 0:
+      #   print("[STEP = %d] --> [TRAINING LOSS = %.4f] --> [TRAINING IOU = %s]" % (step, float(loss_value), mean_class_iou))
+      #   file_path = 'ae_model_step_{}.h5'.format(step)
+      #   tf.keras.models.save_model(model = autoencoder_model, filepath = file_path, overwrite = False, include_optimizer = True)
 
-      progBar.update(step*batch_size, values=values)
-
-    progBar.update(num_training_samples, values=values, finalize=True)
+      progBar.add(batch_size, values)
 
     print(test_iou)
+
+    mean_class_iou = json.dumps(mean_iou) #JSONify the mean iou list containing mean iou for each class
     
     # FOR TRAINING -> Uncomment during actual training
-#   if epoch % 10 == 0:
-#   	print("[EPOCH = %d] --> [TRAINING LOSS = %.4f] --> [TRAINING IOU = %s]" % (epoch, float(loss_value), mean_class_iou))
-#   	file_path = 'ae_model_epoch_{}.h5'.format(epoch)
-#   	tf.keras.models.save_model(model = autoencoder_model, filepath = file_path, overwrite = False, include_optimizer = True)
+    if epoch % 2 == 0 and epoch:
+      # TODO: this print statement outputs a lot of info (training iou list and training loss list)
+      # so correct this to only 1 value eac
+      # print("[EPOCH = %d] --> [TRAINING LOSS = %.4f] --> [TRAINING IOU = %s]" % (epoch, float(loss_value), mean_class_iou))
+
+      file_path = 'ae_model_epoch_{}.h5'.format(epoch)
+
+      # TODO: this saves the model it isn't same a checkpoint (saw this on the tf site)
+      # if loading and resuming works then all is good otherwise take a look again
+      tf.keras.models.save_model(model = autoencoder_model, filepath = file_path, overwrite = False, include_optimizer = True)
