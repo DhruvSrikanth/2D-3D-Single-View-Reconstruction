67c1b3333d12dbbafcff162090c8d5d3bb435ac8
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2021-03-04 01:55:04 +0530

    "JSON Files"

---
+++ install_requirements.py
@@ -0,0 +1,18 @@
+# Requirements for 3D Reconstruction code based on Pix2Vox
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Tensorflow - (2.4.0)
+# pip install tensorflow==2.4.0
+
+# CUDA - (11.0.2)
+# https://developer.nvidia.com/cuda-11.0-download-archive?target_os=Windows&target_arch=x86_64&target_version=10&\
+# target_type=exelocal
+
+# CUDNN - (8.0.4):
+# https://developer.nvidia.com/rdp/cudnn-archive
+
+# https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#installwindows
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+# Unpack Datasets .tgz files with
+# tar xzvf your-file-name.tgz
