8698294322f42235c099ec4d2f6ba06adc5bd963
surajbidnur <63632653+surajbidnur@users.noreply.github.com>
2021-03-29 15:00:13 +0530

    added cuda cudnn install commands

--- commands.py
+++ commands.py
@@ -66,3 +66,18 @@
 # Tensorboard
 # tensorboard --logdir=logs/gradient_tape
 
+# ---------------------------CUDA Installation commands ubuntu 18.04 lts------------------------------------------#
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+# sudo apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
+# sudo apt-get update
+# sudo apt-get -y install cuda
+
+# --------------------------------------cuDNN installation commands -------------------------------------------------#
+# wget https://developer.download.nvidia.com/compute/machine-learning/cudnn/secure/8.0.4/11.0_20200923/cudnn-11.0-linux-x64-v8.0.4.30.tgz
+# tar -xzvf cudnn-11.0-linux-x64-v8.0.4.30.tgz
+# sudo cp cuda/include/cudnn*.h /usr/local/cuda/include 
+# sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64 
+# sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
