77c2269f7e325d802c2aa136aaeb5f8e6a2d6ea3
Dhruv Srikanth <51223342+DhruvSrikanth@users.noreply.github.com>
2021-08-13 12:04:17 +0530

    Update data.py

--- data.py
+++ data.py
@@ -121,9 +121,10 @@ def tf_data_generator2(file_list, mode = 'Train'):
     elif mode == 'Inference':
         img, voxel = file_list[0], file_list[1]
         rgba_in = Image.open(img)
-        # background = Image.new("RGB", rgba_in.size, (255, 255, 255))
-        # background.paste(rgba_in, mask=rgba_in.split()[3])  # 3 is the alpha channel
+        background = Image.new("RGB", rgba_in.size, (255, 255, 255))
+        background.paste(rgba_in, mask=rgba_in.split()[3])  # 3 is the alpha channel
         rendering_image = cv2.resize(np.array(rgba_in).astype(np.float32), (224, 224)) / 255.
+        rendering_image = cv2.cvtColor(rendering_image, cv2.COLOR_BGRA2BGR)
 
         with open(voxel, 'rb') as f:
             volume = binvox_rw.read_as_3d_array(f)
