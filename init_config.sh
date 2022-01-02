1cf52f98e01c1c00f1233ac32a89cac2c90fd749
Suraj Bidnur <bidnur.suraj@gmail.com>
2022-01-02 17:51:29 +0530

    updated comments

--- init_config.sh
+++ init_config.sh
@@ -58,14 +58,5 @@ else
   echo "Directory exists"
 fi
 
-# Uncomment the commnds under this statement to setup virtual environment and install TF and other modules using pip
-# Which folder is the virtual env created? (inside src or outside src?)
-
-# ch_dir $SRC_DIR # if virual env should be inside src folder
-# sudo apt install python3-venv
-# python3 -m venv 3dr_venv
-# source 3dr_venv/bin/activate
-# sudo pip install -r src/requirements.txt
-
 echo "Done"
 exit
